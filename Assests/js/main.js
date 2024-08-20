bc9015a6c5b890f2aaf94acd1cb7f176b938955a
deepanshu572 <142653242+deepanshu572@users.noreply.github.com>
2024-08-20 19:14:59 +0530

    update

--- Assests/js/main.js
+++ Assests/js/main.js
@@ -1,4 +1,91 @@
-          
+   
+   async function RegisterWDCUser() {
+    var full_name = document.getElementById("full_name").value; // $("#full_name").val();
+    var User_Email = document.getElementById("User_Email").value; // $("#User_Email").val();
+    var User_Phone = document.getElementById("User_Phone").value; // $("#User_Phone").val();
+    if (full_name == "" || User_Email == "" || User_Phone == "") {
+      alert("Please Fill The Form");
+    } else {
+      const formData = new FormData();
+      formData.append("Type", "002");
+      formData.append("full_name", full_name);
+      formData.append("User_Email", User_Email);
+      formData.append("User_Phone", User_Phone);
+      let req = await fetch("https://kalamacademy.org/learn/leadcaptureapi.php", {
+        method: "POST",
+        body: formData,
+      });
+      let data = await req.json();
+      console.log(data);
+      
+      add_lead( full_name , User_Email , User_Phone , "" , "WDC" );
+      
+      if (data > 0) {
+        location.href = "https://www.kalamacademy.org/thank-you-for-signup-for-php-course/";
+      } else {
+        alert(data);
+      }
+    }
+  }
+  async function RegisterDMUser() {
+    var full_name = document.getElementById("full_name").value; // $("#full_name").val();
+    var User_Email = document.getElementById("User_Email").value; // $("#User_Email").val();
+    var User_Phone = document.getElementById("User_Phone").value; // $("#User_Phone").val();
+    if (full_name == "" || User_Email == "" || User_Phone == "") {
+      alert("Please Fill The Form");
+    } else {
+      const formData = new FormData();
+      formData.append("Type", "003");
+      formData.append("full_name", full_name);
+      formData.append("User_Email", User_Email);
+      formData.append("User_Phone", User_Phone);
+      let req = await fetch("https://kalamacademy.org/learn/leadcaptureapi.php", {
+        method: "POST",
+        body: formData,
+      });
+      let data = await req.json();
+      console.log(data);
+      
+      add_lead( full_name , User_Email , User_Phone , "" , "DM" );
+      
+      if (data > 0) {
+        location.href = "https://www.kalamacademy.org/thank-you-for-signingup/";
+      } else {
+        alert(data);
+      }
+    }
+  }
+
+  
+  async function add_lead( full_name , User_Email , User_Phone , Shop_Size , interested_in ) {
+    
+    const formData1 = new FormData();
+    formData1.append( "Type", "003A" );
+    formData1.append("name", full_name );
+    formData1.append("email", User_Email );
+    formData1.append("mobile", User_Phone );
+    formData1.append("alternate_mobile", 0 );
+    formData1.append("whatsapp", 0 );
+    
+    formData1.append( "interested_in" , interested_in );
+    formData1.append( "source" , "website" );
+    formData1.append( "status" , "Unassigned" );
+    formData1.append( "caller" , '' );
+    formData1.append( "caller_id" , '' );
+    formData1.append( "state" , '' );
+    formData1.append( "city" , '' );
+    
+    formData1.append("Shop_Size", Shop_Size );
+    
+    let req = await fetch("https://teamka.in/crm1/APIs/ash.php", {
+      method: "POST",
+      body: formData1,
+    });
+    let data = await req.json();
+    console.log(data);
+    
+}
+
           document.addEventListener("DOMContentLoaded", function () {
             const sections = document.querySelectorAll("div[id]");
             const navLinks = document.querySelectorAll("#navbar ul li a");
@@ -140,261 +227,6 @@ function checkElement(el) {
     (left + width) <= (window.pageXOffset + window.innerWidth)
   );
 }
-
-// iframe YT code end
-
-// const arrowRight2 = document.getElementById("arrowRight2");
-// const arrowLeft2 = document.getElementById("arrowLeft2");
-// const list2 = document.getElementById("list2");
-// const valueItemNumber2 = document.getElementById("itemNumber2");
-// const valueItemNumberMax2 = valueItemNumber2.ariaValueMax;
-// const defaultItemNumber2 = 3;
-// const initialNumberItems2 = list2.children.length;
-// const itemNumberMess2 = document.getElementById("itemNumberMess2");
-// let itemNumberValue2 = parseInt(valueItemNumber2.value);
-
-
-// const arrowRight = document.getElementById("arrowRight");
-// const arrowLeft = document.getElementById("arrowLeft");
-// const list = document.getElementById("list");
-// const valueItemNumber = document.getElementById("itemNumber");
-// const valueItemNumberMax = valueItemNumber.ariaValueMax;
-// const defaultItemNumber = 3;
-// const initialNumberItems = list.children.length;
-// const itemNumberMess = document.getElementById("itemNumberMess");
-// const wrapper = document.querySelector(".slider__wrapper");
-// const wrapperWidth = wrapper.offsetWidth;
-// const numVisibleItems = 3;
-// let centralItem = 2;
-// let itemNumberValue = parseInt(valueItemNumber.value);
-
-
-// const initialItemNumberValue = itemNumberValue || defaultItemNumber;
-// let itemNumber = calculatePercentItemNumber(initialItemNumberValue);
-// resetItems(itemNumber);
-
-
-// function calculatePercentItemNumber(num) {
-// return num ? 100 / num : 100 / defaultItemNumber;
-// }
-
-// function listNumber(inputNumber) {
-// const message =
-// inputNumber >= 6
-// ? "You reached the maximum number of items"
-// : `Changed to ${inputNumber}`;
-// itemNumberMess.textContent = message;
-// }
-
-// function resetItems(number) {
-// const sliderItems = document.querySelectorAll(".slider__item");
-// sliderItems.forEach((item) => (item.style.width = `${number}%`));
-// itemNumber = number;
-// }
-
-// function calculateCentralItem(numVisibleItems) {
-// const centralItem = Math.ceil(numVisibleItems / 2);
-// return centralItem;
-// }
-
-// function addActiveElement(centralItem, totalVisibleItems) {
-
-// const sliderItems = document.querySelectorAll(".slider__item");
-// sliderItems.forEach((item) =>
-// item.querySelector(".slider__content").classList.remove("active")
-// );
-
-
-// const central = sliderItems[Math.floor(centralItem)];
-// central.querySelector(".slider__content").classList.add("active");
-
-// if (totalVisibleItems % 2 === 0) {
-// const central2 = sliderItems[Math.floor(centralItem) + 1];
-// central2.querySelector(".slider__content").classList.add("active");
-
-// if (totalVisibleItems == 2) {
-// const central3 = sliderItems[Math.floor(centralItem) - 1];
-// central3.querySelector(".slider__content").classList.add("active");
-// }
-// }
-// }
-
-
-// valueItemNumber.addEventListener("input", function () {
-// itemNumberValue = parseInt(valueItemNumber.value);
-// listNumber(itemNumberValue);
-// const newPercentage = calculatePercentItemNumber(itemNumberValue);
-// resetItems(newPercentage);
-// centralItem = calculateCentralItem(itemNumberValue);
-// addActiveElement(centralItem - 1, itemNumberValue);
-// });
-
-// arrowRight.addEventListener("click", moveFirstToEnd);
-// arrowLeft.addEventListener("click", moveLastToStart);
-
-// function moveFirstToEnd() {
-// const firstItem = list.firstElementChild;
-// firstItem.style.marginLeft = `calc(-${itemNumber}%)`;
-
-// if (firstItem) {
-// setTimeout(() => {
-// firstItem.style.marginLeft = "";
-// list.appendChild(firstItem);
-// }, 300);
-// }
-// addActiveElement(centralItem, itemNumberValue);
-// }
-
-// function moveLastToStart() {
-// const lastItem = list.lastElementChild;
-// list.removeChild(lastItem);
-// list.insertBefore(lastItem, list.firstElementChild);
-// const newFirstItem = list.firstElementChild;
-
-// if (newFirstItem) {
-// newFirstItem.style.marginLeft = `calc(-${itemNumber}%)`;
-// setTimeout(() => {
-// newFirstItem.style.marginLeft = "";
-// }, 1);
-// }
-
-// addActiveElement(centralItem - 1, itemNumberValue);
-// }
-
-
-
-
-
-
-//    2nd slider
-
-// const arrowRight2 = document.getElementById("arrowRight2");
-// const arrowLeft2 = document.getElementById("arrowLeft2");
-// const list2 = document.getElementById("list2");
-// const valueItemNumber2 = document.getElementById("itemNumber2");
-// const valueItemNumberMax2 = valueItemNumber2.ariaValueMax;
-// const defaultItemNumber2 = 3;
-// const initialNumberItems2 = list2.children.length;
-// const itemNumberMess2 = document.getElementById("itemNumberMess2");
-// const wrapper2 = document.querySelector(".slider__wrapper2");
-// const wrapperWidth2 = wrapper2.offsetWidth;
-// const numVisibleItems2 = 3;
-// let centralItem2 = 2;
-// let itemNumberValue2 = parseInt(valueItemNumber2.value);
-
-
-// const initialItemNumberValue2 = itemNumberValue2 || defaultItemNumber2;
-// let itemNumber2 = calculatePercentItemNumber(initialItemNumberValue2);
-// resetItems(itemNumber2);
-
-
-// function calculatePercentItemNumber(num) {
-// return num ? 100 / num : 100 / defaultItemNumber2;
-// }
-
-// function listNumber(inputNumber2) {
-// const message2 =
-// inputNumber2 >= 6
-// ? "You reached the maximum number of items"
-// : `Changed to ${inputNumber2}`;
-// itemNumberMess2.textContent = message2;
-// }
-
-// function resetItems(number2) {
-// const sliderItems2 = document.querySelectorAll(".slider__item2");
-// sliderItems2.forEach((item) => (item.style.width = `${number2}%`));
-// itemNumber2 = number2;
-// }
-
-// function calculateCentralItem(numVisibleItems2) {
-// const centralItem2 = Math.ceil(numVisibleItems2 / 2);
-// return centralItem2;
-// }
-
-// function addActiveElement(centralItem2, totalVisibleItems2) {
-
-// const sliderItems2 = document.querySelectorAll(".slider__item2");
-// sliderItems2.forEach((item) =>
-// item.querySelector(".slider__content2").classList.remove("active")
-// );
-
-
-// const central2 = sliderItems2[Math.floor(centralItem2)];
-// central2.querySelector(".slider__content2").classList.add("active");
-
-// if (totalVisibleItems % 2 === 0) {
-// const central2 = sliderItems2[Math.floor(centralItem2) + 1];
-// central2.querySelector(".slider__content2").classList.add("active");
-
-// if (totalVisibleItems2 == 2) {
-// const central3 = sliderItems2[Math.floor(centralItem) - 1];
-// central3.querySelector(".slider__content2").classList.add("active");
-// }
-// }
-// }
-
-
-// valueItemNumber.addEventListener("input", function () {
-// itemNumberValue = parseInt(valueItemNumber.value);
-// listNumber(itemNumberValue);
-// const newPercentage = calculatePercentItemNumber(itemNumberValue);
-// resetItems(newPercentage);
-// centralItem = calculateCentralItem(itemNumberValue);
-// addActiveElement(centralItem - 1, itemNumberValue);
-// });
-
-// arrowRight.addEventListener("click", moveFirstToEnd);
-// arrowLeft.addEventListener("click", moveLastToStart);
-
-// function moveFirstToEnd() {
-// const firstItem = list.firstElementChild;
-// firstItem.style.marginLeft = `calc(-${itemNumber}%)`;
-
-// if (firstItem) {
-// setTimeout(() => {
-// firstItem.style.marginLeft = "";
-// list.appendChild(firstItem);
-// }, 300);
-// }
-// addActiveElement(centralItem, itemNumberValue);
-// }
-
-// function moveLastToStart() {
-// const lastItem = list.lastElementChild;
-// list.removeChild(lastItem);
-// list.insertBefore(lastItem, list.firstElementChild);
-// const newFirstItem = list.firstElementChild;
-
-// if (newFirstItem) {
-// newFirstItem.style.marginLeft = `calc(-${itemNumber}%)`;
-// setTimeout(() => {
-// newFirstItem.style.marginLeft = "";
-// }, 1);
-// }
-
-// addActiveElement(centralItem - 1, itemNumberValue);
-// }
-
-
-
-
-
-
-        document.querySelectorAll('.scrollButton').forEach(button => {
-    button.addEventListener('click', function() {
-        document.getElementById(this.getAttribute('data-target')).scrollIntoView({ behavior: 'smooth' });
-        document.querySelectorAll('.scrollButton').forEach(btn => btn.classList.remove('clicked'));
-        this.classList.add('clicked');
-    });
-});
-
-
-
-
-
-
-
-
 
 // JavaScript code for initializing sliders
 function initializeSlider(wrapperSelector, listSelector, arrowRightSelector, arrowLeftSelector, valueItemNumberSelector, itemNumberMessSelector) {
